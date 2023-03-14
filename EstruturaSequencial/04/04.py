a8d855448c3e1525e2e4b218e8a7668f8b3e97b1
valenca959 <rodrigovenedino@gmail.com>
2023-03-13 23:17:02 -0300

    Resolução da questão 04

---
+++ EstruturaSequencial/04/04.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite a 1° nota: '))
+n2 = float(input('Digite a 2° nota: '))
+n3 = float(input('Digite a 3° nota: '))
+n4 = float(input('Digite a 4° nota: '))
+media = (n1 + n2 + n3 + n4) / 4
+print(f'A sua média foi de {media:.2f} pontos')
