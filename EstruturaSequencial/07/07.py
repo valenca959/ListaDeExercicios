19ee900fffbc9b2003dfe936374e3f7f0f9c0e39
valenca959 <rodrigovenedino@gmail.com>
2023-03-24 23:10:59 -0300

    Resolução da questão 07

---
+++ EstruturaSequencial/07/07.py
@@ -0,0 +1,4 @@
+base = float(input('Qual a base do quadrado em cm? '))
+altura = float(input('Qual a altura do quadrado em cm? '))
+area = base * altura
+print(f'A área do quadrado é de {area}cm² e o seu dobro é de {area*2}cm²')
