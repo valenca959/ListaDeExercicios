054969b56c6e03434e305d718fcf06c815cd90b3
valenca959 <rodrigovenedino@gmail.com>
2023-03-24 23:11:59 -0300

    Resolução da questão 16

---
+++ EstruturaSequencial/16/16.py
@@ -0,0 +1,4 @@
+tamanho = float(input("Digite m² da área a ser pintada: "))
+tinta = tamanho / 18
+valor = tinta * 80
+print(f'Precisaremos de {tinta:.2f}Litros de tinta \nE o valor total será de R${valor:.2f}')
