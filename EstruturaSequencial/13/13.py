30ada1e1c16c597147af755faf5c50106acebaa4
valenca959 <rodrigovenedino@gmail.com>
2023-03-24 23:11:40 -0300

    Resolução da questão 13

---
+++ EstruturaSequencial/13/13.py
@@ -0,0 +1,3 @@
+h = float(input('Qual a altura da pessoa ? '))
+print(f'O peso ideal para um Homem com {h} de altura é {(72.7*h) - 58:.2f}')
+print(f'O peso ideal para uma Mulher com {h} de altura é {(62.1*h) - 44.7:.2f}')
