e938f68af2a2ef82bdfa06aa6ce52def87b3b213
valenca959 <rodrigovenedino@gmail.com>
2023-03-24 23:11:06 -0300

    Resolução da questão 08

---
+++ EstruturaSequencial/08/08.py
@@ -0,0 +1,4 @@
+valor_horas = float(input('Quanto você ganha por hora? '))
+horas = float(input('Quantas horas trabalhadas no mês? '))
+salario = valor_horas * horas
+print(f'O total do seu sálario no mês foi de R${salario:.2f}')
