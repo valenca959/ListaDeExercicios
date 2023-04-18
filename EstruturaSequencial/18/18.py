72da2ea31ed82e414d5d49175a7cefe2063510ca
valenca959 <rodrigovenedino@gmail.com>
2023-04-18 17:14:25 -0300

    Resolução da questão 18

---
+++ EstruturaSequencial/18/18.py
@@ -0,0 +1,3 @@
+tamanho = float(input('Digite o tamanho do arquivo:[MB] '))
+velocidade = float(input('Digite a velocidade da internet:[Mbps]'))
+print(f'O download demorará {tamanho/velocidade} segundos.')
