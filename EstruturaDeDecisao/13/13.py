9954f5d978050afd18bae8b10b5968b2f42b8028
valenca959 <rodrigovenedino@gmail.com>
2023-04-20 14:09:44 -0300

    Resolução da questão 13

---
+++ EstruturaDeDecisao/13/13.py
@@ -0,0 +1,17 @@
+dia = int(input('Digite um número da semana(1 ao 7): '))
+if dia == 1:
+    print('Domingo!')
+elif dia == 2:
+    print('Segunda!')
+elif dia == 3:
+    print('Terça!')
+elif dia == 4:
+    print('Quarta!')
+elif dia == 5:
+    print('Quinta!')
+elif dia == 6:
+    print('Sexta!')
+elif dia == 7:
+    print('Sábado!')
+else:
+    print('Número inválido')
