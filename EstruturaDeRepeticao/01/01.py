f92fa88eaefb00ec943919eddf8e729815541c77
Rodrigo Valença <125774269+valenca959@users.noreply.github.com>
2023-05-02 21:06:44 -0300

    Delete EstruturaDeRepeticao/01 directory

--- EstruturaDeRepeticao/01/01.py
+++
@@ -1,7 +0,0 @@
-while True:
-    numero = int(input('Digite um número de 0 a 10: '))
-    if 0 <= numero <= 10:
-        break
-    else:
-        print('valor válido')
-print(f'Você digitou {numero}')
