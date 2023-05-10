7ffd60fa975ca2b9b3098ff4fe78078daeed6b35
valenca959 <rodrigovenedino@gmail.com>
2023-05-10 20:34:15 -0300

    Resolução da questão 13

---
+++ EstruturaDeRepeticao/13/13.py
@@ -0,0 +1,8 @@
+base = int(input("Digite o valor da base: "))
+expoente = int(input("Digite o valor do expoente: "))
+cont = 0
+produto = 1
+while cont < expoente:
+    produto = produto*base
+    cont = cont + 1
+print(f'{base} elevado a {expoente} = {produto}')
